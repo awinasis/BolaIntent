9357bc263a42521d1b770e4a67c50cc32149ed6e
abdullah winasis <acefmjokam@gmail.com>
2020-03-06 14:46:32 +0700

    initial-commit

---
+++ app/src/main/java/com/example/bolabola/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.bolabola;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        },2500);
+    }
+}
